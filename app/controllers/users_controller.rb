7c8e6bd48f9b85d1eedf688e6bcad100e8062d04
jschack94 <jschack94@gmail.com>
2020-04-21 18:07:36 -0500

    controller details

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def index
+      render json: User.all
+    end
+  
+    def create
+      user = User.new(username: params[:username], password: params[:password])
+      if user.valid?
+        user.save
+        render json: user
+      else
+        render json: {error: 'user could not be created'}
+      end
+    end
+  
+    def show
+      user = User.find(params[:id])
+      render json: user.notes
+    end
+  end
